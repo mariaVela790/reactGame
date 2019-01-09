bd0e5732bfe72719db534e153f6e6a0fbb402878
Maria Vela <maria.vela790@gmail.com>
2019-01-09 16:08:22 -0600

    Updates

---
+++ precache-manifest.5aac010c90c73932304f00db75d9c3df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d29fbd345ffce575370",
+    "url": "/reactGame/static/css/main.7ab2338e.chunk.css"
+  },
+  {
+    "revision": "6d29fbd345ffce575370",
+    "url": "/reactGame/static/js/main.6d29fbd3.chunk.js"
+  },
+  {
+    "revision": "ec8eef1e198ebb0cbe4b",
+    "url": "/reactGame/static/js/1.ec8eef1e.chunk.js"
+  },
+  {
+    "revision": "b9fb290e6bd9fcbdc29b",
+    "url": "/reactGame/static/js/runtime~main.b9fb290e.js"
+  },
+  {
+    "revision": "341115088a30cd709157d219e65c7ca6",
+    "url": "/reactGame/index.html"
+  }
+];
